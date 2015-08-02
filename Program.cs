6b05867d4fe80a4c3b55cb36e0b6f76abc0a59f9
cxehud <ehud.shavit@checkmarx.com>
2015-08-02 15:54:48 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpRequestMessage request = new HttpRequestMessage();
+
+            Console.WriteLine(request.ToString());
+
+            string a = "dsfsadf";
+
+            a = null;
+
+
+            string tokenDetailsString = string.Empty;
+
+            using (StreamReader sr = new StreamReader(""))
+            {
+                tokenDetailsString = sr.ReadToEnd();
+            }
+        }
+    }
+}
